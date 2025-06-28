7241750638906152d100003e6f8804bf8b3eef23
Finidi47 <finidisaid@gmail.com>
2025-06-28 15:55:10 +0300

    Adding quotes dynamically

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -36,3 +36,61 @@ function showRandomQuote(){
 // listen  to the newQuote button and show the quotes
 const showBtn = document.getElementById('newQuote');
 showBtn.addEventListener('click', showRandomQuote)
+
+// ===============================SECTION 2========================================
+
+function createAddQuoteForm(){
+    const formContainer = document.createElement('div');
+    formContainer.id = 'addQuoteFormContainer';
+
+    const quoteInput = document.createElement('input');
+    quoteInput.id = 'newQuoteText';
+    quoteInput.type = 'text';
+    quoteInput.placeholder = 'Enter a new quote';
+
+    const categoryInput = document.createElement('input');
+    categoryInput.id = 'newQuoteCategory';
+    categoryInput.type = 'text';
+    categoryInput.placeholder = 'Enter quote category'
+
+    const formBtn = document.createElement('button');
+    formBtn.innerHTML = 'Add Quote';
+
+    // listen to the Add quote button. Get the inputs and validate them
+    formBtn.addEventListener('click', () => {
+        const newQuoteText = quoteInput.value.trim();
+        const newQuoteCategory = categoryInput.value.trim();
+
+        if (newQuoteText === '' || newQuoteCategory === ''){
+            alert('please fill in both fields');
+            return;
+        }
+
+        // Create new quote object from the inputs
+        const newQuoteObject = {
+            text: newQuoteText,
+            category: newQuoteCategory
+        };
+
+        // Add the new object to the main array
+        quotes.push(newQuoteObject);
+
+        // clear th input fields 
+        quoteInput.value = '';
+        categoryInput.value = '';
+
+        // provide feedback when form is submitted
+        alert('Quote added successfully');
+    })
+
+    formContainer.appendChild(quoteInput);
+    formContainer.appendChild(categoryInput);
+    formContainer.appendChild(formBtn);
+
+    document.body.appendChild(formContainer);
+}
+
+
+
+const addBtn = document.getElementById('addNewQuote');
+addBtn.addEventListener('click', createAddQuoteForm);
